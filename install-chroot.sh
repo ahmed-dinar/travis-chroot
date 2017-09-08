0e7765622c738ead9bf1a3eb308eddab5276787b
ahmed-dinar <madinar.cse@gmail.com>
2017-09-08 16:05:50 +0600

    comments

--- install-chroot.sh
+++ install-chroot.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-
+# install debootstrap
 sudo apt-get update
 sudo apt-get install dchroot debootstrap
 sudo mkdir /JAIL/
 
+# setup schroot config
 sudo chmod 777 /etc/schroot/schroot.conf
 sudo cat schroot.conf >> /etc/schroot/schroot.conf
 
+# install chroot
 sudo debootstrap --variant=buildd --arch amd64 xenial /JAIL/ http://mirror.math.princeton.edu/pub/ubuntu/
+
+# mount proc to chroot
 sudo mount proc /JAIL/proc -t proc
 ls /JAIL
 
+# setup chroot, check gcc, insall java, check java
 sudo cp setup-chroot.sh /JAIL
 sudo cat /JAIL/setup-chroot.sh
 sudo chmod +x /JAIL/setup-chroot.sh
 sudo chroot /JAIL ./setup-chroot.sh
 
-sudo mount proc /JAIL/proc -t proc
+# make programm run directory inside chroot
 sudo mkdir /JAIL/home/runs
+ls /JAIL/home/runs
--- setup-chroot.sh
+++ setup-chroot.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# check if gcc exists
 gcc --version
+
+# install java 8
 apt-get -y install default-jdk
 java -version
+
+# exit from chroot
 exit
