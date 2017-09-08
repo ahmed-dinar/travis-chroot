6e196114759fb896db4068498c986eaddf93fd84
justoj-me <justoj.com@gmail.com>
2017-09-08 15:31:34 +0600

    Create install-chroot.sh

---
+++ install-chroot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ls
+sudo apt-get update
+sudo apt-get install dchroot debootstrap
+sudo mkdir /JAIL/
+
+sudo chmod 777 /etc/schroot/schroot.conf
+sudo cat schroot.conf >> /etc/schroot/schroot.conf
+sudo cat /etc/schroot/schroot.conf
+
+sudo debootstrap --variant=buildd --arch amd64 xenial /JAIL/ http://mirror.math.princeton.edu/pub/ubuntu/
+sudo mount proc /JAIL/proc -t proc
+ls /JAIL
+
+sudo cp setup-chroot.sh /JAIL
+sudo cat /JAIL/setup-chroot.sh
+sudo chmod +x /JAIL/setup-chroot.sh
