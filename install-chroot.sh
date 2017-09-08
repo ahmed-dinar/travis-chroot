9f9de574d57d74ab2a85fbdf639080eaeed31fbe
ahmed-dinar <madinar.cse@gmail.com>
2017-09-08 17:18:27 +0600

    change chroot dir

--- install-chroot.sh
+++ install-chroot.sh
@@ -3,25 +3,25 @@
 # install debootstrap
 sudo apt-get update
 sudo apt-get install dchroot debootstrap
-sudo mkdir /JAIL/
+sudo mkdir /var/SECURITY/JAIL/
 
 # setup schroot config
 sudo chmod 777 /etc/schroot/schroot.conf
 sudo cat schroot.conf >> /etc/schroot/schroot.conf
 
 # install chroot
-sudo debootstrap --variant=buildd --arch amd64 xenial /JAIL/ http://mirror.math.princeton.edu/pub/ubuntu/
+sudo debootstrap --variant=buildd --arch amd64 xenial /var/SECURITY/JAIL/ http://mirror.math.princeton.edu/pub/ubuntu/
 
 # mount proc to chroot
-sudo mount proc /JAIL/proc -t proc
+sudo mount proc /var/SECURITY/JAIL/proc -t proc
 ls /JAIL
 
 # setup chroot, check gcc, insall java, check java
 sudo cp setup-chroot.sh /JAIL
-sudo cat /JAIL/setup-chroot.sh
-sudo chmod +x /JAIL/setup-chroot.sh
+sudo cat /var/SECURITY/JAIL/setup-chroot.sh
+sudo chmod +x /var/SECURITY/JAIL/setup-chroot.sh
 sudo chroot /JAIL ./setup-chroot.sh
 
 # make programm run directory inside chroot
-sudo mkdir /JAIL/home/runs
-ls /JAIL/home/runs
+sudo mkdir /var/SECURITY/JAIL/home/runs
+ls /var/SECURITY/JAIL/home/runs
